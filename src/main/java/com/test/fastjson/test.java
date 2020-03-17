6e964cab24267e03d4b61617af0bf3c710990b2b
huzhen <12345678>
2020-03-17 20:58:23 +0800

    测试引入本地jar包，不通过maven

---
+++ src/main/java/com/test/fastjson/test.java
@@ -0,0 +1,12 @@
+package com.test.fastjson;
+
+import com.alibaba.fastjson.*;
+
+/**
+ * Created by Administrator on 2020/3/17.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.printf("hello");
+    }
+}
